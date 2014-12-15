25ab6d16fa2f5e15f3fd98a6852dc67132a03d98
ArnasBinkauskas <arnas.binkauskas@gmail.com>
2014-12-15 18:57:05 +0000

    made And gates drawable

--- Circumator/src/CircuitDraw.java
+++ Circumator/src/CircuitDraw.java
@@ -25,8 +25,8 @@ public class CircuitDraw extends JFrame {
 	 public void paint(Graphics g) {
 		    //Here is how we used to draw a square with width
 		    //of 200, height of 200, and starting at x=50, y=50.
-		   Inverter b = new Inverter();
-		   b.setCordinates(80, 80);
+		   And b = new And();
+		   b.setCordinates(80, 200);
 		   b.paint(g);
 	}
 }
--- Circumator/src/logicComponent/oneBit/primitive/And.java
+++ Circumator/src/logicComponent/oneBit/primitive/And.java
@@ -1,4 +1,6 @@
 package logicComponent.oneBit.primitive;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.util.Map;
 
 import logicComponent.LogicComponent;
@@ -19,11 +21,14 @@ public class And extends LogicComponent implements Pushable{
 	int width = 50;
 	int height = 25;
 	
-	int inCordX = xCord - width/10;
-	int inCordY = yCord + height/2;
+	int inXCordX = xCord - width/10;
+	int inXCordY = yCord + height/4;
 	
-	int outCordX = xCord + width + width/10;
-	int outCordY = yCord + height/2;
+	int inYCordX = xCord - width/10;
+	int inYCordY = yCord + (height/4) * 3;
+	
+	int outUCordX = xCord + width + width/10;
+	int outUCordY = yCord + height/2;
 	
 	int gateDelay = 1;
 	/**
@@ -39,10 +44,11 @@ public class And extends LogicComponent implements Pushable{
 		output.put("u", null);
 		xCord = 0;
 		yCord = 0;
+		setCordinates(0,0);
 		label = "And";
 	}
 	
-	public And(ENode in_nodeX, ENode in_nodeY, SNode out_node){
+	public And(ENode in_nodeX, ENode in_nodeY, SNode out_node, int x, int y){
 		super(2,1);
 		input.put("x", null);
 		input.put("y", null);
@@ -50,6 +56,7 @@ public class And extends LogicComponent implements Pushable{
 		in_nodeX.plugTo(this, "x");
 		in_nodeY.plugTo(this, "y");
 		out_node.plugTo(this, "u");
+		setCordinates(x,y);
 	}
 	
 	public void setGateDelay(int value){
@@ -65,5 +72,26 @@ public class And extends LogicComponent implements Pushable{
 			out.setValue((inX.getValue() && inY.getValue()));
 		}
 	}
+	
+	@Override
+	public void updateInOut(){
+		inXCordX = xCord - width/10;
+		inXCordY = yCord + height/4;
+		
+		inYCordX = xCord - width/10;
+		inYCordY = yCord + (height/4) * 3;
+		
+		outUCordX = xCord + width + width/10;
+		outUCordY = yCord + height/2;
+	}
+	
+	 public void paint(Graphics g) {
+		    g.setColor(Color.black);
+		    g.drawRect(xCord,yCord, width,height);
+		    g.drawString(label, xCord + 8, yCord + 18);
+		    g.drawLine(xCord, inXCordY, inXCordX, inXCordY);
+		    g.drawLine(xCord, inYCordY, inYCordX, inYCordY);
+		    g.drawLine(outUCordX - width/10, outUCordY, outUCordX, outUCordY);
+	 }
 	
 }
